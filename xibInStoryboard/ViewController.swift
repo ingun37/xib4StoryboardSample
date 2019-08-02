67c5d9147aa1146e16a3502820f5ceb419115e9e
ingun <ingun37@gmail.com>
2019-08-03 05:04:42 +0900

    Initial Commit

---
+++ xibInStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xibInStoryboard
+//
+//  Created by ingun on 03/08/2019.
+//  Copyright © 2019 ingun37. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
